f8d59aa5d10514e5a5dcec9de521583247bf4da5
raibowwwhoozy <raibowwwhoozy@gmail.com>
2017-02-06 20:02:30 +0300

    Examples

---
+++ Example/ChildEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class ChildEntity : BaseClass
+{
+
+	void Update()
+	{
+		m_Health--; 
+		if (m_Health <= 0) {
+			TestBaseClass.DeadMinion.Invoke (this.gameObject); // Делаем инвок того что моб уме (( звук, минус колчиество общее, место в гонке)
+			ChangeSpeed ( -5f); // Показываем что в наследниках можно вызывать метоеды, даже если они не указаны
+		}
+	}
+
+	public override void DoSomething ()
+	{
+		Destroy (this.gameObject);
+	}
+
+	 
+}
---
+++ Example/TestBaseClass.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TestBaseClass : MonoBehaviour {
+	public static Action ScoreChange;
+	public static Action<GameObject> DeadMinion;
+	public List<GameObject> enemies; //Если будем хранить врагов, то пригодится
+
+	public static TestBaseClass instance;
+
+
+	private int m_Score;
+	public  int Score
+	{
+		set{
+			m_Score = value;
+			DeadMinion.Invoke(this.gameObject);
+		}
+		get{
+			return m_Score;
+		}
+	}
+
+	void Awake()
+	{
+		if (instance == null) 
+		{
+			instance = this;
+		}
+		else 
+		{
+			Destroy (instance.gameObject);
+			instance = this;
+		}
+	}
+
+	//Подписываем на эвенты
+	void OnEnable()
+	{
+		ScoreChange += DoSomethingWithScore;
+		DeadMinion += DeadSound;
+	}
+
+	void OnDisable()
+	{
+		ScoreChange -= DoSomethingWithScore;
+		DeadMinion -= DeadSound;
+	}
+
+	private void DoSomethingWithScore(){
+		
+		Debug.Log ("ScoreChanges");
+	}
+
+	private void DeadSound(GameObject obj){
+		enemies.Remove (obj);
+		Debug.Log ("Minion is Dead");
+	}
+}
